7420615e84619ed100146ff0bd581876772fd973
zufichris <zufichris@gmail.com>
2024-10-07 03:37:01 +0400

    building individual pages

---
+++ src/constants/products.tsx
@@ -0,0 +1,48 @@
+export const products = [
+  {
+    href: "https://github.com/zufichris/multivendor_ecommerce_app",
+    title: "Multi-vendor E-commerce App",
+    description:
+      "ECommerce platform that allows multiple sellers to manage their own storefronts while providing a seamless shopping experience for customers and easy payments for customers.",
+    thumbnail: '/images/projects/e2.jfif',
+    images: ['/images/projects/e2.jfif', '/images/projects/e1.jpg'],
+    stack: [
+      "Javascript",
+      "NodeJS",
+      "ReactJS",
+      "MongoDB",
+      "Socket.io",
+      "HTML/CSS",
+      "Tailwindcss",
+    ],
+    slug: "ecommerce",
+  },
+  {
+    href: "https://github.com/zufichris/messenger-",
+    title: "Messenger",
+    description:
+      "Feature-rich messaging application designed for seamless communication between users including real-time messaging, push notifications and media sharing.",
+    thumbnail: "/images/projects/mess1.png",
+    images: ["/images/projects/mess1.png","/images/projects/mess2.png"],
+    stack: [
+      "Typescript",
+      "NodeJS",
+      "ReactJS",
+      "MongoDB",
+      "Socket.io",
+      "HTML/CSS",
+      "Tailwindcss",
+    ],
+    slug: "messenger",
+  },
+  {
+    href: "https://github.com/zufichris/CRM-system",
+    title: "Customer relationship management (CRM) system",
+    description:
+      "CRM system designed to streamline customer interactions and enhance relationship management. The system features a centralized database for easy access to customer information, automated communication tools for timely follow-ups, and detailed analytics for data-driven insights. ",
+    thumbnail:'/images/projects/crm1.jpg',
+    images: ['/images/projects/crm1.jpg','/images/projects/crm2.jpg'],
+    stack: ["C#","PostgreSQL","React","AWS", "Tailwindcss"],
+    slug: "crm",
+  },
+];
---
+++ src/constants/socials.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBrandLinkedin,
+  IconBrandX,
+  IconBrandGithub,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://linkedin.com/in/zufi-christian-5b216a248",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://github.com/zufichris",
+    label: "Github",
+    icon: IconBrandGithub,
+  },
+  {
+    href: "https://x.com/zufichris_",
+    label: "X",
+    icon: IconBrandX,
+  },
+];
