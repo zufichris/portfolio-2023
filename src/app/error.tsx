83bb3abfdf8a964eab5cdab72d3bd716a4e7476a
zufichris <zufichris@gmail.com>
2024-10-07 23:56:36 +0400

    render resume

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,10 +2,10 @@
 import React from "react";
 const error = ({ error, reset }: { error: Error; reset: () => void }) => {
   return (
-    <div>
-      <h1>An Error Occured</h1>
-      <p>{error.message ?? ""}</p>
-      <button className="bg-blue-600 rounded-md px-2 py-1" onClick={reset}>
+    <div className="h-screen p-5 w-screen flex justify-center items-center flex-col">
+      <h1 className="font-bold">An Error Occured</h1>
+      <p className="text-red-600">{error.message ?? ""}</p>
+      <button className="bg-blue-600 rounded-md px-3 py-1 text-white" onClick={reset}>
         Retry
       </button>
     </div>
