16a7279cf1fd375152c7c08ba9eae4b0ef6b1a7e
zufichris <zufichris@gmail.com>
2024-10-08 12:32:32 +0400

    Update Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -53,7 +53,7 @@ export const SingleProduct = ({ product }: { product: Product }) => {
         ))}
       </div>
       <div className="flex lg:flex-row justify-between items-center flex-col mt-20">
-        <Heading className="font-black mb-2 pb-1"> {product.title}</Heading>
+        <Heading className="font-semibold mb-2 pb-1"> {product.title}</Heading>
         <div className="flex space-x-2 md:mb-1 mt-2 md:mt-0">
           {product.stack?.map((stack: string) => (
             <span
